06357c700224362afa092487a66e54a789bd348b
Logan Kane <logan.kane1@gmail.com>
2018-12-06 13:54:46 -0800

    cs
    source code

---
+++ Assets/Colorstroop/Scripts/CommonScripts/InfoButton.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class InfoButton : MonoBehaviour 
+{
+	public Button btnInfo;
+	public string NavigationURL;
+
+	/// <summary>
+	/// Start this instance.
+	/// </summary>
+	void Start()
+	{
+		btnInfo.onClick.AddListener(() => 
+		                             {
+			if (InputManager.instance.canInput ()) {
+				InputManager.instance.DisableTouchForDelay ();
+				InputManager.instance.AddButtonTouchEffect ();
+                Application.OpenURL("https://rabbiteargames.com/");
+
+            }
+		});
+	}
+}
---
+++ Assets/Colorstroop/Scripts/CommonScripts/RateButton.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class RateButton : MonoBehaviour
+{
+	//  The button to rate. Assigned from inspector.
+	public Button btnRate;
+
+	public string PlayStoreURL = "https://play.google.com/store/apps/details?id=com.rabbiteargames.colorstroop";
+
+	public string AmazonStoreURL = "https://play.google.com/store/apps/details?id=com.rabbiteargames.colorstroop";
+
+	public string AppStoreURL = "https://itunes.apple.com/app/id1442036674";
+
+	bool isAmazon = false;
+
+	/// <summary>
+	/// Start this instance.
+	/// </summary>
+	void Start()
+	{
+		btnRate.onClick.AddListener(() =>
+		                             {
+			if (InputManager.instance.canInput ()) {
+				InputManager.instance.DisableTouchForDelay ();
+				InputManager.instance.AddButtonTouchEffect ();
+
+				#if UNITY_ANDROID
+				if(!isAmazon) {
+                    Application.OpenURL(AmazonStoreURL);
+				}
+				else {
+                    Application.OpenURL(PlayStoreURL);
+				}
+				#elif UNITY_IOS
+				Application.OpenURL(AppStoreURL);
+				#elif UNITY_EDITOR
+				Application.OpenURL("https://rabbiteargames.com/");
+				#endif
+			}
+		});
+	}
+}
